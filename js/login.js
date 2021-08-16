0fccb4590a3fb81dafd1308e37b4ff7b6c003cb2
aleblen <alejandroblengini@gmail.com>
2021-08-16 18:03:12 -0400

    google bien

--- js/login.js
+++ js/login.js
@@ -39,6 +39,7 @@ function onSignIn(googleUser) {
   // The ID token you need to pass to your backend:
   var id_token = googleUser.getAuthResponse().id_token;
   console.log("ID Token: " + id_token);
+  location.href="index.html";
 }
 
 
