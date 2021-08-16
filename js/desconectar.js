96f3ae6ca7ea42de6a7381654e6b4c063475be6d
aleblen <alejandroblengini@gmail.com>
2021-08-16 18:51:30 -0400

    desconectar google

--- js/desconectar.js
+++ js/desconectar.js
@@ -1,5 +1,18 @@
 function desconectar() {
     localStorage.clear;
     sessionStorage.clear;
+    signOut();
     location.href="login.html";
+}
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
+function onLoad() {
+    gapi.onLoad(`auth2`, function(){
+        gapi.auth2.init();
+    });
 }
