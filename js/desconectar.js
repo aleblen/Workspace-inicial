b8599d1625af21c28cf6411969354bd9f5b376d4
aleblen <alejandroblengini@gmail.com>
2021-08-17 19:48:40 -0400

    aberrr

--- js/desconectar.js
+++ js/desconectar.js
@@ -1,19 +1,19 @@
-/*function onLoad() {
-    gapi.onLoad(`auth2`, function(){
-        gapi.auth2.init();
-    });
-}
+
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
   }
-  */
+  function onLoad() {
+    gapi.load(`auth2`, function(){
+        gapi.auth2.init();
+    });
+}
 function desconectar() {
     localStorage.clear;
     sessionStorage.clear;
-    //signOut();
+    signOut();
     location.href="login.html";
 }
 
