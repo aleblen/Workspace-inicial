ffe4bebb50f514af6a1f57d06098fc8a5382e83e
aleblen <alejandroblengini@gmail.com>
2021-08-17 20:02:14 -0400

    coso

--- js/desconectar.js
+++ js/desconectar.js
@@ -1,9 +1,9 @@
 
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+  console.log("User signed out.");
+  });
   }
   function onLoad() {
     gapi.load(`auth2`, function(){
@@ -16,6 +16,7 @@ function desconectar() {
     signOut();
     location.href="login.html";
 }
+
 
 
 
